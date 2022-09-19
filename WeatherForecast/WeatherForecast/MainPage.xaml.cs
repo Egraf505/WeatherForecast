7e43d80429c45494b9c5faa5a1306f14fb6cd4c8
Egraf505 <egor.leskov.03@mail.ru>
2022-09-19 22:56:06 +0300

    Add get of location

--- WeatherForecast/WeatherForecast/MainPage.xaml.cs
+++ WeatherForecast/WeatherForecast/MainPage.xaml.cs
@@ -5,14 +5,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Threading;
 
 namespace WeatherForecast
 {
     public partial class MainPage : ContentPage
     {
+        CancellationTokenSource cts;
         public MainPage()
         {
             InitializeComponent();
+
+            GetCurrentLocation();
+        }
+
+        async Task GetCurrentLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                cts = new CancellationTokenSource();
+                var location = await Geolocation.GetLocationAsync(request, cts.Token);
+
+                if (location != null)
+                {
+                    this.Content = new Label() { Text = location.Latitude + " " + location.Longitude };
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "Ok");              
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            if(cts != null && !cts.IsCancellationRequested)
+                cts.Cancel();
+            base.OnDisappearing();
         }
     }
 }
